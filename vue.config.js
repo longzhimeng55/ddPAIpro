1f0c0daa400f086d7680594541cda69ca561ab7c
LAPTOP-R8QF9OIT\lenovo <413138632@qq.com>
2020-07-31 10:22:28 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -47,6 +47,18 @@ module.exports = {
       options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
       return options
     })
+    //压缩图片
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        mozjpeg: { progressive: true, quality: 65 },
+        optipng: { enabled: false },
+        pngquant: { quality: [0.65, 0.9], speed: 4 },
+        gifsicle: { interlaced: false },
+        webp: { quality: 75 }
+      })
   },
   devServer: {
     port: port,
